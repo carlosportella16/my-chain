eed30ae628a286179b6dabc61eb0dc95dd5169ae
carlosportella16 <carlosportella16@hotmail.com>
2023-10-07 02:57:09 -0300

    Transactions, StringUtil, Block and MyChain finished

--- src/main/java/project/StringUtil.java
+++ src/main/java/project/StringUtil.java
@@ -1,7 +1,12 @@
 package project;
 
+import com.google.gson.GsonBuilder;
+import project.transactions.Transaction;
+
 import java.security.*;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 
 // Generating a digital fingerprint with SHA256
 public class StringUtil {
@@ -12,6 +17,7 @@ public class StringUtil {
     public static String applySha256(String input) {
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
             byte[] hash = digest.digest(input.getBytes("UTF-8"));
             StringBuffer hexString = new StringBuffer();
             for (int i = 0; i < hash.length; i++) {
@@ -55,7 +61,40 @@ public class StringUtil {
         }
     }
 
+    //Short hand helper to turn Object into a json string
+    public static String getJson(Object o) {
+        return new GsonBuilder().setPrettyPrinting().create().toJson(o);
+    }
+
+    //Returns difficulty string target, to compare to hash. eg difficulty of 5 will return "00000"
+    public static String getDificultyString(int difficulty) {
+        return new String(new char[difficulty]).replace('\0', '0');
+    }
+
     public static String getStringFromKey(Key key) {
         return Base64.getEncoder().encodeToString(key.getEncoded());
     }
+
+    public static String getMerkleRoot(ArrayList<Transaction> transactions) {
+        int count = transactions.size();
+
+        List<String> previousTreeLayer = new ArrayList<String>();
+        for(Transaction transaction : transactions) {
+            previousTreeLayer.add(transaction.transactionId);
+        }
+        List<String> treeLayer = previousTreeLayer;
+
+        while(count > 1) {
+            treeLayer = new ArrayList<String>();
+            for(int i=1; i < previousTreeLayer.size(); i+=2) {
+                treeLayer.add(applySha256(previousTreeLayer.get(i-1) + previousTreeLayer.get(i)));
+            }
+            count = treeLayer.size();
+            previousTreeLayer = treeLayer;
+        }
+
+        String merkleRoot = (treeLayer.size() == 1) ? treeLayer.get(0) : "";
+        return merkleRoot;
+    }
+
 }
--- src/main/java/project/transactions/Transaction.java
+++ src/main/java/project/transactions/Transaction.java
@@ -10,11 +10,11 @@ public class Transaction {
     public String transactionId; // This is also the hash of the transaction
     public PublicKey sender; // senders address/public key.
     public PublicKey reciepient; // Recipients address/public key.
-    public float value;
-    public byte[] signature; // this is to prevent anybody else from spending funds in our wallet.
+    public float value; // Contains the amount we wish to send to the recipient.
+    public byte[] signature; // This is to prevent anybody else from spending funds in our wallet.
 
-    public ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
-    public ArrayList<TransactionOutput> outputs = new ArrayList<TransactionOutput>();
+    public ArrayList<TransactionInput> inputs = new ArrayList<>();
+    public ArrayList<TransactionOutput> outputs = new ArrayList<>();
 
     private static int sequence = 0; // a rough count of how many transactions have been generated.
 
@@ -26,7 +26,72 @@ public class Transaction {
         this.inputs = inputs;
     }
 
-    // This Calculates the transaction hash (which will be used as its Id)
+    public boolean processTransaction() {
+
+        if(verifySignature() == false) {
+            System.out.println("#Transaction Signature failed to verify");
+            return false;
+        }
+
+        //Gathers transaction inputs (Making sure they are unspent):
+        for(TransactionInput i : inputs) {
+            i.UTXO = MyChain.UTXOs.get(i.transactionOutputId);
+        }
+
+        //Checks if transaction is valid:
+        if(getInputsValue() < MyChain.minimumTransaction) {
+            System.out.println("Transaction Inputs too small: " + getInputsValue());
+            System.out.println("Please enter the amount greater than " + MyChain.minimumTransaction);
+            return false;
+        }
+
+        //Generate transaction outputs:
+        float leftOver = getInputsValue() - value; //get value of inputs then the left over change:
+        transactionId = calulateHash();
+        outputs.add(new TransactionOutput( this.reciepient, value,transactionId)); //send value to recipient
+        outputs.add(new TransactionOutput( this.sender, leftOver,transactionId)); //send the left over 'change' back to sender
+
+        //Add outputs to Unspent list
+        for(TransactionOutput o : outputs) {
+            MyChain.UTXOs.put(o.id , o);
+        }
+
+        //Remove transaction inputs from UTXO lists as spent:
+        for(TransactionInput i : inputs) {
+            if(i.UTXO == null) continue; //if Transaction can't be found skip it
+            MyChain.UTXOs.remove(i.UTXO.id);
+        }
+
+        return true;
+    }
+
+    public float getInputsValue() {
+        float total = 0;
+        for(TransactionInput i : inputs) {
+            if(i.UTXO == null) continue; //if Transaction can't be found skip it, This behavior may not be optimal.
+            total += i.UTXO.value;
+        }
+        return total;
+    }
+
+    public void generateSignature(PrivateKey privateKey) {
+        String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(reciepient) + Float.toString(value)	;
+        signature = StringUtil.applyECDSASig(privateKey,data);
+    }
+
+    public boolean verifySignature() {
+        String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(reciepient) + Float.toString(value)	;
+        return StringUtil.verifyECDSASig(sender, data, signature);
+    }
+
+    public float getOutputsValue() {
+        float total = 0;
+        for(TransactionOutput o : outputs) {
+            total += o.value;
+        }
+        return total;
+    }
+
     private String calulateHash() {
         sequence++; //increase the sequence to avoid 2 identical transactions having the same hash
         return StringUtil.applySha256(
@@ -34,72 +99,5 @@ public class Transaction {
                         StringUtil.getStringFromKey(reciepient) +
                         Float.toString(value) + sequence
         );
-    }
-
-    // Signs all the data we dont wish to be tampered with
-    public void generateSignature(PrivateKey privateKey) {
-        String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(reciepient) + Float.toString(value);
-        signature = StringUtil.applyECDSASig(privateKey, data);
-    }
-
-    // Verifies the data we signed hasnt been tampered with
-    public boolean verifySignature(){
-        String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(reciepient) + Float.toString(value);
-        return StringUtil.verifyECDSASig(sender, data, signature);
-    }
-
-    public boolean processTransaction() {
-        if(!verifySignature()) {
-            System.out.println("#Transaction Signature failed to verify");
-            return false;
-        }
-
-        // Gather transaction inputs (Make sure they are unspent)
-        for(TransactionInput i : inputs) {
-            i.UTXO = MyChain.UTXOs.get(i.transactionOutputId);
-        }
-
-        // Check if transaction is valid
-        if(getInputsValue() < MyChain.minimumTransaction) {
-            System.out.println("#Transaction Inputs to small: " + getInputsValue());
-            return false;
-        }
-
-        // Generate transaction outputs:
-        float leftOver = getInputsValue() - value; // get value of inputs then the leftover change:
-        transactionId = calulateHash();
-        outputs.add(new TransactionOutput(this.reciepient, value, transactionId)); // send value to recipient
-        outputs.add(new TransactionOutput(this.reciepient, leftOver, transactionId)); // send the leftover 'change' back to sender
-
-        // Add outputs to Unspent list
-        for(TransactionOutput o : outputs) {
-            MyChain.UTXOs.put(o.id, o);
-        }
-
-        // Remove transaction inputs from UTXO lists as spent:
-        for(TransactionInput i : inputs) {
-            if(i.UTXO == null) continue; // if transaction cant be found skip it
-            MyChain.UTXOs.remove(i.UTXO.id);
-        }
-        return true;
-    }
-
-    // returns sum of inputs (UTXOs) values
-    public float getInputsValue(){
-        float total = 0;
-        for(TransactionInput i : inputs) {
-            if(i.UTXO == null) continue;
-            total += i.UTXO.value;
-        }
-        return total;
-    }
-
-    // returns sum of outputs:
-    public float getOutputsValue() {
-        float total = 0;
-        for(TransactionOutput o : outputs) {
-            total += o.value;
-        }
-        return total;
     }
 }
